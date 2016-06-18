a9b6c2953e6ab9f7be34b2c37153d27d55ee385b
alphayax <alphayax@gmail.com>
2016-06-18 15:05:27 +0200

    Update tests

--- test/model/HelpTest.php
+++ test/model/HelpTest.php
@@ -6,6 +6,7 @@ class HelpTest extends \PHPUnit_Framework_TestCase {
     public function testConstruct() {
 
         $optionList = new \alphayax\utils\cli\model\OptionList();
+        $optionList->add( new \alphayax\utils\cli\model\Option( 'a', 'aa', 'AAA'));
 
         $help = new \alphayax\utils\cli\model\Help();
         $help->setDescription( "Description");
