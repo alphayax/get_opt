ed8c86b43c7ba487593594be9c3567ad316d131c
alphayax <alphayax@gmail.com>
2016-06-18 15:01:07 +0200

    Update tests

--- test/GetOptTest.php
+++ test/GetOptTest.php
@@ -14,4 +14,20 @@ class GetOptTest extends PHPUnit_Framework_TestCase {
         $a->parse();
     }
 
+    /**
+     * @throws \alphayax\utils\cli\exception\MissingArgException
+     */
+    public function testException() {
+        
+        $this->setExpectedException( \alphayax\utils\cli\exception\MissingArgException::class);
+
+        $a = new \alphayax\utils\cli\GetOpt();
+        $a->setDescription( 'This script is a tiny example to show library features');
+        $a->addLongOpt( 'dry-run', 'Dry Run mode', true, true);
+        $a->addShortOpt( 'd', 'Debug mode');
+        $a->addOpt( 'v', 'verbose', "Verbose Mode");
+        $a->parse();
+    }
+
+
 }
